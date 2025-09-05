2819c528ffd4d1f80ee333af8d6bc4f2af1add30
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-09-05 16:23:04 +0400

    fixed

--- python-import_modules/3-infinite_add.py
+++ python-import_modules/3-infinite_add.py
@@ -1,10 +1,10 @@
 #!/usr/bin/python3
 
 import sys
+if __name__ == "__main__":
+    argv_sum = 0
 
-argv_sum = 0
+    for i in range(1, len(sys.argv)):
+        argv_sum += int(sys.argv[i])
 
-for i in range(1, len(sys.argv)):
-    argv_sum += int(sys.argv[i])
-
-print("{}".format(argv_sum))
+    print("{}".format(argv_sum))
