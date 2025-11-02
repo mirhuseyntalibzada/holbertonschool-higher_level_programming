e4b7b1abd6470ff2e478aaffa7f597a91cb234e6
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-11-02 19:45:09 +0000

    3-square.py

--- python-classes/3-square.py
+++ python-classes/3-square.py
@@ -2,3 +2,18 @@
 """This module defines a Square class."""
 
 
+class Square:
+    """Represents a geometric square."""
+
+    def __init__(self, size=0):
+        """Initializes a Square instance with its size."""
+        if not isinstance(size, int):
+            raise TypeError("size must be an integer")
+        if size < 0:
+            raise ValueError("size must be >= 0")
+
+        self.__size = size
+
+    def area(self):
+        """Calculates the area of the square."""
+        return self.__size * self.__size
