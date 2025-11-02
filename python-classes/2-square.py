5f37eb8c8ad78d7211385ff1a32f2a72398d8ba4
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-11-02 19:33:53 +0000

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -2,3 +2,14 @@
 """This module defines a Square class."""
 
 
+class Square:
+    """Represents a geometric square."""
+
+    def __init__(self, size=0):
+        """Initializes a Square instance with its size."""
+        if type(size) != int:
+            raise TypeError("size must be an integer")
+        if size <= 0:
+            raise TypeError("size must be >= 0")
+        
+        self.__size = size
