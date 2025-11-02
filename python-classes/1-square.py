3ec7e16f2ea1b2b78ead94bca8b17f1d9eef33f2
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-11-02 19:23:08 +0000

    1-square.py

---
+++ python-classes/1-square.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""This module defines a Square class."""
+
+
+class Square:
+    """Represents a geometric square."""
+
+    def __init__(self, size):
+        """Initializes a Square instance with its size."""
+        self.__size = size
