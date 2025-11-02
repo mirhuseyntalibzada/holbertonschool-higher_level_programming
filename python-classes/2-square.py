ba7e1f88a4698e17bd1bca5973a5c94151cd6d2c
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-11-02 19:37:53 +0000

    2-square.py

--- python-classes/2-square.py
+++ python-classes/2-square.py
@@ -9,7 +9,39 @@ class Square:
         """Initializes a Square instance with its size."""
         if type(size) != int:
             raise TypeError("size must be an integer")
-        if size <= 0:
-            raise TypeError("size must be >= 0")
+        if size < 0:
+            raise ValueError("size must be >= 0")
         
         self.__size = size
+
+my_square_1 = Square(3)
+print(type(my_square_1))
+print(my_square_1.__dict__)
+
+my_square_2 = Square()
+print(type(my_square_2))
+print(my_square_2.__dict__)
+
+try:
+    print(my_square_1.size)
+except Exception as e:
+    print(e)
+
+try:
+    print(my_square_1.__size)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_3 = Square("3")
+    print(type(my_square_3))
+    print(my_square_3.__dict__)
+except Exception as e:
+    print(e)
+
+try:
+    my_square_4 = Square(-89)
+    print(type(my_square_4))
+    print(my_square_4.__dict__)
+except Exception as e:
+    print(e)
