4437bb81d50b04c664b1b850b9742a14484cb2ef
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-11-11 14:00:46 +0000

    fixed code style

--- python-inheritance/0-lookup.py
+++ python-inheritance/0-lookup.py
@@ -3,6 +3,7 @@
 A module that returns the list of available attributes and methods of an object
 """
 
+
 def lookup(obj):
     """
     Returns the list of available attributes and methods of an object
