54d4df456f09477f239ec4641ddf015df352520f
mirhuseyntalibzada <mirhuseyntalibzade2004@gmail.com>
2025-11-11 14:33:45 +0000

    SQL_introduction

---
+++ SQL_introduction/4-first_table.sql
@@ -0,0 +1,7 @@
+-- CREATE TABLE IF NOT EXISTS
+-- Creates table first_table (id INT, name VARCHAR(256)) in the current database
+CREATE TABLE IF NOT EXISTS first_table (
+  id INT,
+  name VARCHAR(256)
+);
+
---
+++ SQL_introduction/7-insert_value.sql
@@ -0,0 +1,4 @@
+-- INSERT A NEW ROW INTO first_table
+-- Adds: id = 89, name = 'Best School'
+INSERT INTO first_table (id, name)
+VALUES (89, 'Best School');
